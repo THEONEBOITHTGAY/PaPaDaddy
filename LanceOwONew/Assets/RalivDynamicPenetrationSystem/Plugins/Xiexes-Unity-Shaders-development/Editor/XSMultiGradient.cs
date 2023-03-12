26f0e86be485f35e2c13aacc16c442e980bcb2d3
Golden Fox <97137383+GolenFox@users.noreply.github.com>
2023-03-12 11:19:02 -0500

    Revert " 9f5as98rj6596yh9we5"
    This reverts commit 3aea849124e8becd7e6c842b226973e5678f41d6.

--- LanceOwONew/Assets/RalivDynamicPenetrationSystem/Plugins/Xiexes-Unity-Shaders-development/Editor/XSMultiGradient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-namespace XSToonDynamicPenetration {
-	public class XSMultiGradient : ScriptableObject {
-		public string uniqueName = "New Gradient";
-		public List<Gradient> gradients = new List<Gradient>();
-		public List<int> order = new List<int>();
-	}
-}
